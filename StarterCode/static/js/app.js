fadc3c9971b01567f950ad5f766b0b6b7270b0c8
Eric Viera <ericviera@Erics-MBP.home>
2020-10-25 11:50:15 -0400

    3rd commit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,8 +1,46 @@
+//# Step 1: Plotly
+
+//1. Use the D3 library to read in `samples.json`.
+
 // Fetch the JSON data and console log it
-d3.json("Data/samples.json").then(function(data) {
+d3.json("Data/samples.json").then((data) => {
   console.log(data);
 });
 
 // Promise Pending
 const dataPromise = d3.json("Data/samples.json");
 console.log("Data Promise: ", dataPromise);
+
+
+//2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+//* Use `sample_values` as the values for the bar chart.
+//* Use `otu_ids` as the labels for the bar chart.
+//* Use `otu_labels` as the hovertext for the chart.
+
+
+
+
+
+//3. Create a bubble chart that displays each sample.
+
+//* Use `otu_ids` for the x values.
+
+//* Use `sample_values` for the y values.
+
+//* Use `sample_values` for the marker size.
+
+//* Use `otu_ids` for the marker colors.
+
+//* Use `otu_labels` for the text values.
+
+
+
+//4. Display the sample metadata, i.e., an individual's demographic information.
+
+//5. Display each key-value pair from the metadata JSON object somewhere on the page.
+
+
+
+//6. Update all of the plots any time that a new sample is selected.
+
+//Additionally, you are welcome to create any layout that you would like for your dashboard.
